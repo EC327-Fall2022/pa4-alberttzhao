96392fd32e9665d6493c31d8634e78acdabbdf80
Albert Zhao <albertz@crc-dot1x-nat-10-239-254-74.bu.edu>
2022-12-07 10:28:48 -0500

    Step 2, a lot of errors generated

--- Model.cpp
+++ Model.cpp
@@ -14,48 +14,78 @@ Model::Model()
     C1_initial_location.x = 1;
     C1_initial_location.y = 20;
     PokemonCenter* center1_ptr = new PokemonCenter(1, 1, 100, C1_initial_location);
-    object_ptrs[2] = center1_ptr;
-    center_ptrs[0] = center1_ptr;
+    //object_ptrs[2] = center1_ptr;
+    //center_ptrs[0] = center1_ptr;
+
+    //PA4: 
+    object_ptrs.push_back(center1_ptr);
+    active_ptrs.push_back(center1_ptr);
+    center_ptrs.push_back(center1_ptr);
 
     //C2 is a PokemonCenter:
     Point2D C2_initial_location;
     C2_initial_location.x = 10;
     C2_initial_location.y = 20;
     PokemonCenter* center2_ptr = new PokemonCenter(2, 2, 200, C2_initial_location);
-    object_ptrs[3] = center2_ptr;
-    center_ptrs[1] = center2_ptr;
+    // object_ptrs[3] = center2_ptr;
+    // center_ptrs[1] = center2_ptr;
+
+    //PA4:
+    object_ptrs.push_back(center2_ptr);
+    active_ptrs.push_back(center2_ptr);
+    center_ptrs.push_back(center2_ptr);
 
     //T1 is a Trainer:
     Point2D T1_initial_location;
     T1_initial_location.x = 5;
     T1_initial_location.y = 1;
     Trainer* trainer1_ptr = new Trainer("Ash", 1, 'T', 1, T1_initial_location);
-    object_ptrs[0] = trainer1_ptr;
-    trainer_ptrs[0] = trainer1_ptr; 
+    // object_ptrs[0] = trainer1_ptr;
+    // trainer_ptrs[0] = trainer1_ptr; 
+
+    //PA4:
+    object_ptrs.push_back(trainer1_ptr);
+    active_ptrs.push_back(trainer1_ptr);
+    trainer_ptrs.push_back(trainer1_ptr);
     
     //T2 is a trainer:
     Point2D T2_initial_location;
     T2_initial_location.x = 10;
     T2_initial_location.y = 1;
     Trainer* trainer2_ptr = new Trainer("Misty", 2, 'T', 2, T2_initial_location);
-    object_ptrs[1] = trainer2_ptr;
-    trainer_ptrs[1] = trainer2_ptr;
+    // object_ptrs[1] = trainer2_ptr;
+    // trainer_ptrs[1] = trainer2_ptr;
+
+    //PA4:
+    object_ptrs.push_back(trainer2_ptr);
+    active_ptrs.push_back(trainer2_ptr);
+    trainer_ptrs.push_back(trainer2_ptr);
 
     //G1 is a PokemonGym:
     Point2D G1_initial_location;
     G1_initial_location.x = 0;
     G1_initial_location.y = 0;
     PokemonGym* gym1_ptr = new PokemonGym(2, 1, 2, 3, 1, G1_initial_location); //remember to change it back to 10
-    object_ptrs[4] = gym1_ptr;
-    gym_ptrs[0] = gym1_ptr;
+    // object_ptrs[4] = gym1_ptr;
+    // gym_ptrs[0] = gym1_ptr;
+
+    //PA4: 
+    object_ptrs.push_back(gym1_ptr);
+    active_ptrs.push_back(gym1_ptr);
+    gym_ptrs.push_back(gym1_ptr);
 
     //G2 is a PokemonGym:
     Point2D G2_initial_location;
     G2_initial_location.x = 5;
     G2_initial_location.y = 5;
     PokemonGym* gym2_ptr = new PokemonGym(2, 5, 7.5, 4, 2, G2_initial_location); // remember to change it back to 20
-    object_ptrs[5] = gym2_ptr;
-    gym_ptrs[1] = gym2_ptr;
+    // object_ptrs[5] = gym2_ptr;
+    // gym_ptrs[1] = gym2_ptr;
+
+    //PA4: 
+    object_ptrs.push_back(gym2_ptr);
+    active_ptrs.push_back(gym2_ptr);
+    gym_ptrs.push_back(gym2_ptr);
 
     //others:
     num_objects = 8; 
@@ -69,15 +99,23 @@ Model::Model()
     WildPokemon1_location.x = 10;
     WildPokemon1_location.y = 12;
     WildPokemon* WildPokemon1_ptr = new WildPokemon("Eevee", 5.0, 5.0, false, 1, WildPokemon1_location); //using default constructor? 
-    object_ptrs[6] = WildPokemon1_ptr;
-    WildPokemon_ptrs[0] = WildPokemon1_ptr;
+    // object_ptrs[6] = WildPokemon1_ptr;
+    // WildPokemon_ptrs[0] = WildPokemon1_ptr;
+
+    object_ptrs.push_back(WildPokemon1_ptr);
+    active_ptrs.push_back(WildPokemon1_ptr);
+    WildPokemon_ptrs.push_back(WildPokemon1_ptr);
 
     Point2D WildPokemon2_location;
     WildPokemon2_location.x = 15;
     WildPokemon2_location.y = 5;
     WildPokemon* WildPokemon2_ptr = new WildPokemon("Charizard", 5.0, 5.0, false, 2, WildPokemon2_location); //using default constructor? 
-    object_ptrs[7] = WildPokemon2_ptr;
-    WildPokemon_ptrs[1] = WildPokemon2_ptr;
+    // object_ptrs[7] = WildPokemon2_ptr;
+    // WildPokemon_ptrs[1] = WildPokemon2_ptr;
+
+    object_ptrs.push_back(WildPokemon2_ptr);
+    active_ptrs.push_back(WildPokemon2_ptr);
+    WildPokemon_ptrs.push_back(WildPokemon2_ptr);
     
 
 
@@ -95,23 +133,48 @@ Model::~Model()
 
 Trainer* Model::GetTrainerPtr(int id)
 {
-    if(trainer_ptrs[id-1] != NULL)
+    // From PA3: 
+    // if(trainer_ptrs[id-1] != NULL)
+    // {
+    //     //return if pointer is found:
+    //     return trainer_ptrs[id-1];
+    // }
+    // else
+    // {
+    //     //return 0 if pointer is not found:
+    //     return 0;
+    // }
+
+    //PA4: modified from above:
+    auto trainer_ptrs_front = trainer_ptrs.begin();
+    if(next(*trainer_ptrs_front, id) != NULL)
     {
-        //return if pointer is found:
-        return trainer_ptrs[id-1];
+        return next(*trainer_ptrs_front, id);
     }
     else
     {
-        //return 0 if pointer is not found:
         return 0;
     }
+
 }
 
 PokemonCenter* Model::GetPokemonCenterPtr(int id)
 {
-    if(center_ptrs[id-1] != NULL)
+    // From PA3: 
+    // if(center_ptrs[id-1] != NULL)
+    // {
+    //     return center_ptrs[id-1];
+    // }
+    // else
+    // {
+    //     return 0;
+    // }
+
+    //PA4: modified from above:
+    auto center_ptrs_front = center_ptrs.begin();
+    if(next(*center_ptrs_front, id) != NULL)
     {
-        return center_ptrs[id-1];
+        return next(*center_ptrs_front, id);
     }
     else
     {
@@ -121,9 +184,21 @@ PokemonCenter* Model::GetPokemonCenterPtr(int id)
 
 PokemonGym* Model::GetPokemonGymPtr(int id)
 {
-    if(gym_ptrs[id-1] != NULL)
+    // From PA3: 
+    // if(gym_ptrs[id-1] != NULL)
+    // {
+    //     return gym_ptrs[id-1];
+    // }
+    // else
+    // {
+    //     return 0;
+    // }
+
+    //PA4: modified from above:
+    auto gym_ptrs_front = gym_ptrs.begin();
+    if(next(*gym_ptrs_front , id) != NULL)
     {
-        return gym_ptrs[id-1];
+        return next(*gym_ptrs_front , id);
     }
     else
     {
@@ -135,9 +210,21 @@ PokemonGym* Model::GetPokemonGymPtr(int id)
 //PA4:
 WildPokemon* Model::GetWildPokemonPtr(int id)
 {
-    if(WildPokemon_ptrs[id-1] != NULL)
+    // From PA3: 
+    // if(WildPokemon_ptrs[id-1] != NULL)
+    // {
+    //     return WildPokemon_ptrs[id-1];
+    // }
+    // else
+    // {
+    //     return 0;
+    // }
+
+    //PA4: modified from above:
+    auto WildPokemon_ptrs_front = WildPokemon_ptrs.begin();
+    if(next(*WildPokemon_ptrs_front , id) != NULL)
     {
-        return WildPokemon_ptrs[id-1];
+        return next(*WildPokemon_ptrs_front , id);
     }
     else
     {
@@ -154,39 +241,91 @@ bool Model::Update()
     //From Thomas:
     bool valid_thomas = false;
 
-    for(int i = 0; i < num_objects; i++)
+    // PA3: 
+    // for(int i = 0; i < num_objects; i++)
+    // {
+
+
+    //     if(object_ptrs[i]->Update() == true)
+    //     {
+    //         //write two more for loops and use exit(0):
+    //         int num_gyms_without_battles = 0;
+    //         for(int j = 0; j < num_gyms; j++)
+    //         {
+    //             if(gym_ptrs[j]->passed() == true)
+    //             {
+    //                 num_gyms_without_battles = num_gyms_without_battles + 1;
+    //             }
+    //         }
+
+    //         if(num_gyms_without_battles == num_gyms)
+    //         {
+    //             cout << "GAME OVER: You win! All battles done! " << endl;
+    //             exit(0);
+    //         }
+
+    //         valid_thomas = true; //thomas fixed
+    //     }
+
+    // }
+
+    //PA4 modified:
+    int object_size = object_ptrs.size();
+    for(int i = 1; i <= object_size; i++)
     {
-        if(object_ptrs[i]->Update() == true)
+        auto object_ptrs_front1 = object_ptrs.begin();
+        if(next(*object_ptrs_front1 , i)->Update() == true)
         {
-            //write two more for loops and use exit(0):
             int num_gyms_without_battles = 0;
-            for(int j = 0; j < num_gyms; j++)
+
+            int gym_size = gym_ptrs.size();
+            for(int j=1; j<= gym_size; j++)
             {
-                if(gym_ptrs[j]->passed() == true)
+                auto gym_ptrs_front1 = gym_ptrs.begin();
+                if(next(*gym_ptrs_front1 , j)->passed() == true)
                 {
-                    num_gyms_without_battles = num_gyms_without_battles + 1;
+                    num_gyms_without_battles++;
                 }
             }
 
-            if(num_gyms_without_battles == num_gyms)
+            if(num_gyms_without_battles == gym_size)
             {
                 cout << "GAME OVER: You win! All battles done! " << endl;
                 exit(0);
             }
 
-            valid_thomas = true; //thomas fixed
+            valid_thomas = true;
         }
-
     }
     CheckTrainerWildPokemon();
 
+    // PA3: 
+    // int num_trainers_fainted = 0;
+    // for(int k = 0; k < num_trainers; k++)
+    // {
+    //     if(trainer_ptrs[k]->HasFainted() == true)
+    //     {   
+    //         num_trainers_fainted = num_trainers_fainted + 1;
+    //     }
+    // }
 
+    // if(num_trainers_fainted == num_trainers)
+    // {
+    //     cout << "GAME OVER: You lose! All of your Trainers' Pokemon have fainted! " << endl;
+    //     exit(0);
+    // }
+
+    
+    //PA4 modified:
     int num_trainers_fainted = 0;
-    for(int k = 0; k < num_trainers; k++)
+    
+    int trainer_size = trainer_ptrs.size();
+    for(int k = 0; k <= trainer_size; k++)
     {
-        if(trainer_ptrs[k]->HasFainted() == true)
-        {   
-            num_trainers_fainted = num_trainers_fainted + 1;
+        auto trainer_ptrs_front1 = trainer_ptrs.begin();
+        if(next(*trainer_ptrs_front1 , k)->HasFainted() == true)
+        {
+            num_trainers_fainted++;
         }
     }
 
@@ -196,6 +335,7 @@ bool Model::Update()
         exit(0);
     }
 
+    
     return valid_thomas;
 }
 
@@ -204,11 +344,25 @@ bool Model::Update()
 void Model::Display(View& view)
 {
 
+    //PA3: 
+    // view.Clear();
+
+    
+    // for(int i = 0; i < num_objects; i++)
+    // {
+    //     view.Plot(object_ptrs[i]);
+    // }
+
+    // view.Draw();
+
+
+    //PA4 modified: 
     view.Clear();
 
-    for(int i = 0; i < num_objects; i++)
+    for(int i = 1; i <= object_ptrs.size(); i++)
     {
-        view.Plot(object_ptrs[i]);
+        auto object_ptrs_front2 = object_ptrs.begin();
+        view.Plot(next(*object_ptrs_front2 , i));
     }
 
     view.Draw();
@@ -217,11 +371,22 @@ void Model::Display(View& view)
 
 void Model::ShowStatus(){
 
-    cout << "Time: " << time << endl; 
+    // PA3: 
 
-    for (int i = 0; i < num_objects; i++)
+    // cout << "Time: " << time << endl; 
+
+    // for (int i = 0; i < num_objects; i++)
+    // {
+    //     object_ptrs[i]->ShowStatus();
+    // }
+
+    //PA4 modified: 
+    cout << "Time: " << time << endl;
+
+    for(int i = 1; i <= object_ptrs.size(); i++)
     {
-        object_ptrs[i]->ShowStatus();
+        auto object_ptrs_front3 = object_ptrs.begin();
+        next(*object_ptrs_front3 , i)->ShowStatus();
     }
 }
 
--- Model.h
+++ Model.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
+#include <list>
+#include <iterator>
+
 
 #include "GameObject.h"
 #include "Trainer.h"
@@ -22,19 +26,38 @@ private:
 
     int time;
 
-    GameObject* object_ptrs[10];
+    /*
+    PA4 part 2:
+    First, replace your array of Game_object pointers named “object_ptrs” 
+    with a linked list named object_ptrs and another called active_ptrs. 
+    */
+    list<GameObject*> object_ptrs;
+
+    /*
+    The object_ptrs list will point to all of the Game_Objects that exist, 
+    while the active_ptrs list will point to all of the Game_Objects that are still 
+    alive and must be updated and displayed. 
+    */
+
+    // GameObject* object_ptrs[10];
+    list<GameObject*> active_ptrs;
     int num_objects;
 
-    Trainer* trainer_ptrs[10];
+
+    // Trainer* trainer_ptrs[10];
+    list<Trainer*> trainer_ptrs;
     int num_trainers;
 
-    PokemonCenter* center_ptrs[10];
+    // PokemonCenter* center_ptrs[10];
+    list<PokemonCenter*> center_ptrs;
     int num_centers;
 
-    PokemonGym* gym_ptrs[10];
+    // PokemonGym* gym_ptrs[10];
+    list<PokemonGym*> gym_ptrs;
     int num_gyms;
     
-    WildPokemon* WildPokemon_ptrs[10];
+    // WildPokemon* WildPokemon_ptrs[10];
+    list<WildPokemon*> WildPokemon_ptrs;
     int num_WildPokemons;
 
 public:
